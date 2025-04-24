871dab1643bc106e3d6fee56f5b8c1a7157c6581
李晏瑋 <tony.li@hcinnovation.tw>
2025-04-24 20:15:44 +0800

    feat: tainwindcss install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -9,5 +10,11 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
