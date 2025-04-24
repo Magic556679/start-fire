cd794e7780af28c36d92ce0c5a8f483d6c381652
李晏瑋 <tony.li@hcinnovation.tw>
2025-04-24 20:10:33 +0800

    add index vue

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-gray-800 mb-4">Hi，我是晏瑋</h1>
+    <p class="text-lg text-gray-600">這是我的個人作品集網站</p>
+  </div>
+</template>
